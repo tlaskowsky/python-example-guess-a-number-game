92c8b2a00aafdd52cc53e27e398013db33ea3847
Terumi Laskowsky <terumi@credosec.com>
2023-12-01 12:56:15 -1000

    Update app.py
    fixed random = 1 issue

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, session
 import random
+import os
 
 app = Flask(__name__)
-number = random.randint(1, 100)
+app.secret_key = os.urandom(24)  # or set a fixed secret key
 
 @app.route('/', methods=['GET', 'POST'])
 def guess():
+    if 'number' not in session:
+        session['number'] = random.randint(1, 100)
     message = ''
     if request.method == 'POST':
         guess = int(request.form['guess'])
-        if guess < number:
+        if guess < session['number']:
             message = 'Too low!'
-        elif guess > number:
+        elif guess > session['number']:
             message = 'Too high!'
         else:
-            message = 'You guessed it!'
+            message = 'You guessed it! The number was ' + str(session['number'])
+            session.pop('number', None)  # Reset the number for a new game
     return render_template_string('''
         <html>
             <body>
